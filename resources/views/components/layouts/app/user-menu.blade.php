ebc7d40239d9bd11a85bfc57ed481f57677731af
Swilam <swilaaam@gmail.com>
2024-02-29 17:25:23 +0100

    - Add link to admin panel in menu

--- resources/views/components/layouts/app/user-menu.blade.php
+++ resources/views/components/layouts/app/user-menu.blade.php
@@ -14,6 +14,14 @@
 
             <ul tabindex="0" class="mt-3 z-[1] py-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                 <li class="py-1">
+                    @if (auth()->user()->isAdmin())
+                        <x-link href="{{ route('filament.admin.pages.dashboard') }}" class="!px-2">
+                            <div class="flex flex-row gap-1">
+                                @svg('dashboard', 'h-4 text-primary-500 m-1 stroke-primary-500')
+                                {{ __('Admin Panel') }}
+                            </div>
+                        </x-link>
+                    @endif
                     <x-link href="{{ route('filament.dashboard.pages.dashboard') }}" class="!px-2">
                         <div class="flex flex-row gap-1">
                             @svg('dashboard', 'h-4 text-primary-500 m-1 stroke-primary-500')
@@ -21,7 +29,7 @@
                         </div>
                     </x-link>
                 </li>
-                <li class="py-1">
+                <li class="">
                     <x-link href="{{ route('logout') }}" class="!px-2" onclick="event.preventDefault(); this.closest('form').submit();">
                         <div class="flex flex-row gap-1">
                             @svg('logout', 'h-4 m-1 stroke-primary-500')
