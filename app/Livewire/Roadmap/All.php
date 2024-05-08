131beaed64f7f36781321075fb2bc3b9034aa473
Swilam <swilaaam@gmail.com>
2024-05-08 09:13:05 +0200

    Improve roadmap done view.

--- app/Livewire/Roadmap/All.php
+++ app/Livewire/Roadmap/All.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Roadmap;
 
 use App\Services\RoadmapManager;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,16 +11,19 @@ class All extends Component
 {
     use WithPagination;
 
+    #[Url]
+    public $done = false;
+
     public function render(RoadmapManager $roadmapManager)
     {
         return view('livewire.roadmap.all', [
-            'items' => $roadmapManager->getAll()
+            'items' => $this->done ? $roadmapManager->getCompleted() : $roadmapManager->getAll(),
         ]);
     }
 
     public function upvote(int $id, RoadmapManager $roadmapManager)
     {
-        if (!auth()->check()) {
+        if (! auth()->check()) {
             return redirect()->route('login');
         }
 
@@ -28,7 +32,7 @@ class All extends Component
 
     public function removeUpvote(int $id, RoadmapManager $roadmapManager)
     {
-        if (!auth()->check()) {
+        if (! auth()->check()) {
             return redirect()->route('login');
         }
 
