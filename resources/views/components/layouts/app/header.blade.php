774081db33a266b860e32af7151ce1c5b9ca8abd
Swilam <swilaaam@gmail.com>
2024-09-10 22:19:52 +0200

    - Fix missing translations. - Add initial plans/products on homepage as inspiration.

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -23,7 +23,7 @@
                 <x-layouts.app.user-menu></x-layouts.app.user-menu>
             @else
                 <x-link class="hidden md:block text-primary-50" href="{{route('login')}}">{{ __('Login') }}</x-link>
-                <x-button-link.secondary elementType="a" href="#plans">{{ __('Get started') }}</x-button-link.secondary>
+                <x-button-link.secondary elementType="a" href="#pricing">{{ __('Get started') }}</x-button-link.secondary>
             @endauth
         </div>
     </div>
