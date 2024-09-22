8683a9d0331a6f15df1ea2b5ca739074716fe43a
Swilam <swilaaam@gmail.com>
2024-09-22 15:23:43 +0200

    - Various code cleanups & removing redundant parts. - Update libraries.

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\ServiceProvider;
 
 return [
 
@@ -189,38 +188,6 @@ return [
     'recaptcha_enabled' => false,
 
     'multiple_subscriptions_enabled' => false,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-        App\Providers\ConfigProvider::class,
-        App\Providers\BladeProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\Filament\DashboardPanelProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
-
-    ])->toArray(),
 
     /*
     |--------------------------------------------------------------------------
